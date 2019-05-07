124cfd5d40b133fc4373bdf8443425f230188f9d
Mr7F <git@sep.tf>
2019-05-07 11:07:01 +0200

    package

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='cmdargs',
+      version='0.1',
+      description='Parse the command line arguments with just a decorator',
+      url='https://github.com/Mister7F/cmdargs',
+      author='Mister7F',
+      author_email='-',
+      license='MIT',
+      packages=['.'],
+      zip_safe=False)
